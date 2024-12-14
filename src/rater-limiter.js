d7b3ed1e38923abb97a5b6aa7fb07b3274981c31
Tom Van Dort <tom@vandort.dev>
2024-12-13 21:48:47 -0500

    rate limiter class

--- src/rater-limiter.js
+++ src/rater-limiter.js
@@ -3,36 +3,40 @@ import { Keyv } from "keyv";
 const rateLimitWindow = 10 * 1000; // 10 second window in milliseconds
 const maxRequests = 5; // Allow 5 requests per user per window
 
-let keyv = new Keyv();
+export class RateLimiter {
+  constructor() {
+    this.keyv = new Keyv();
+  }
 
-export async function isUserRateLimited(username) {
-  const key = `rateLimit:${username}`;
+  async isUserRateLimited(username) {
+    const key = `rateLimit:${username}`;
 
-  // Retrieve the current request count and timestamp from Keyv
-  const data = await keyv.get(key);
+    // Retrieve the current request count and timestamp from Keyv
+    const data = await this.keyv.get(key);
 
-  const currentTime = Date.now();
+    const currentTime = Date.now();
 
-  if (data) {
-    const { count, lastRequestTime } = data;
+    if (data) {
+      const { count, lastRequestTime } = data;
 
-    if (currentTime - lastRequestTime > rateLimitWindow) {
-      await keyv.set(key, { count: 1, lastRequestTime: currentTime });
+      if (currentTime - lastRequestTime > rateLimitWindow) {
+        await this.keyv.set(key, { count: 1, lastRequestTime: currentTime });
 
-      return true; // The request is allowed
-    }
+        return true; // The request is allowed
+      }
 
-    if (count < maxRequests) {
-      await keyv.set(key, { count: count + 1, lastRequestTime });
+      if (count < maxRequests) {
+        await this.keyv.set(key, { count: count + 1, lastRequestTime });
 
-      return true; // The request is allowed
+        return true; // The request is allowed
+      } else {
+        return false; // Exceeded the rate limit
+      }
     } else {
-      return false; // Exceeded the rate limit
-    }
-  } else {
-    // If no data exists, create new record
-    await keyv.set(key, { count: 1, lastRequestTime: currentTime });
+      // If no data exists, create new record
+      await this.keyv.set(key, { count: 1, lastRequestTime: currentTime });
 
-    return true; // The request is allowed
+      return true; // The request is allowed
+    }
   }
 }
